6dd048d82119bb64b085841cdb20385284364832
teragoon <teragoon@naver.com>
2012-04-17 14:55:15 +0900

    add redis

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+	, redis = require('redis')
 	, io = require('socket.io');
 
 
@@ -10,8 +11,20 @@ var app = express.createServer()
 	, tserver_idx = {};
 
 socket.configure(function(){
-	socket.set('log level', 4);
+	socket.set('log level', 1);
 });
+
+var rc = new redis.createClient(8080, '211.113.17.102', null);
+
+rc.on('ready', function(){
+	console.log("redis client reday!");
+});
+
+rc.on('err', function(err){
+	console.log('redis client err!');
+	console.dir(err);	
+});
+
 
 // Configuration
 app.configure(function(){
@@ -39,6 +52,30 @@ socket.on('connection', function(con){
 	con.on('getServerMap', function(fn){
 		console.log('con getServerMap -> ');
 		console.dir(server_map);
+		var result = {};
+		
+		rc.smembers("node", function(err, nodes){
+			if(err) console.log(err);
+			else{
+				console.log("-> nodes : ");
+				console.dir(nodes);
+				nodes.forEach(function(reply, i){
+					console.log("-->node: " + nodes[i]);
+					rc.hgetall(nodes[i], function(err, replies){
+						if(err) console.log(err);
+						else{
+							console.log("--> replies: " + nodes[i] + " : ");
+							console.dir(replies);
+							result[nodes[i]] = replies;
+							console.log("->result : ");		
+							console.dir(result);
+						}
+					});
+				});
+			}
+		});
+		
+		
 		
 		fn(server_map);
 		
@@ -84,6 +121,20 @@ socket.on('connection', function(con){
 //		console.log('tserver_idx - >');
 //		console.dir(tserver_idx);
 		console.log('connect - > ' + con.tserverName);
+		
+		for(key in msg){
+			rc.sadd( "service:" + key, JSON.stringify(msg[key]), function(err){
+				if(err) console.log(err);
+			});
+			
+			rc.sadd( "thrift:" + msg[key].serverName, key + ":" + msg[key].port, function(err){
+				if(err) console.log(err);
+			});
+			
+			rc.sadd( "thrift", "thrift:" + msg[key].serverName, function(err){
+				if(err) console.log(err);
+			});
+		}
 	});
 	
 	con.on('njoin', function(msg){
@@ -105,30 +156,68 @@ socket.on('connection', function(con){
 //		console.log('nserver_idx - >');
 //		console.dir(nserver_idx);
 		console.log('connect - > ' + con.nserverName);
+		
+		for(key in msg){
+			rc.hset( "node:" + key, "service:" + msg[key].serviceName, null, function(err){
+				if(err) console.log(err);
+			});
+			
+			rc.sadd( "node", "node:" + key, function(err){
+				if(err) console.log(err);
+			});
+		}
 	});
 	
 	con.on('disconnect', function(){
-		if(!con.tserverName) return;
-		
-		for(key in tservers){
-			for(var i=0; i < tservers[key].length; i++){
-				console.log('-> ' + tservers[key][i].serverName);
-				
-				if(tservers[key][i].serverName == con.tserverName){
-					console.log('del -> ' + tservers[key][i]);
-					tservers[key].splice(i, 1);
-//					tserver_idx[key] = tservers[key].length;
+		if(con.tserverName){
+			rc.smembers("thrift:"+con.tserverName, function(err, replies){
+				if(err) console.log(err);
+				else{
+					console.log("-> replies");
+					console.dir(replies);
+					replies.forEach(function(reply, i){
+						var r = reply.split(":");
+						var value = {serverName: con.tserverName, port: r[1]};
+						
+						rc.srem("service:" + r[0], JSON.stringify(value));						
+						rc.srem("thrift:" + con.tserverName, reply);
+						rc.srem("thrift", con.tserverName);
+					});
+				}
+			});
+			
+			for(key in tservers){
+				for(var i=0; i < tservers[key].length; i++){
+					console.log('-> ' + tservers[key][i].serverName);
+					
+					if(tservers[key][i].serverName == con.tserverName){
+						console.log('del -> ' + tservers[key][i]);
+						tservers[key].splice(i, 1);
+	//					tserver_idx[key] = tservers[key].length;
+					}
 				}
 			}
+			
+			console.log('tservers - >');
+			console.dir(tservers);
+	//		console.log('server_idx - >');
+	//		console.dir(server_idx);
+			console.log("disconnect : " + con.tserverName);
+			
+			con.broadcast.emit('announcement', {server: con.tserverName, action: "disconnected"});
+		}else if(con.nserverName){
+			delete nservers[con.nserverName];
+			delete server_map[con.nserverName];
+			
+			rc.hgetall("node:"+con.nserverName, function(err, replies){
+				replies.forEach(function(reply, i){
+					rc.hdel("node:" + con.nserverName, reply);	
+				});
+				
+			});
+			
+			rc.srem("node", "node" + con.nserverName);
 		}
-		
-		console.log('tservers - >');
-		console.dir(tservers);
-//		console.log('server_idx - >');
-//		console.dir(server_idx);
-		console.log("disconnect : " + con.tserverName);
-		
-		con.broadcast.emit('announcement', {tserver: con.tserverName, action: "disconnected"});
 	})
 });
 
@@ -147,6 +236,32 @@ function getTserverByRR(key, nserverName){
 		tserver_idx[key] = 0;
 		i=0;
 	}
+	
+	rc.incr("thrift:idx:"+key, function(err, idx){
+		console.log("->reply : " + idx);
+		rc.scard("service:" + key, function(err, max){
+			if(err) console.log(err);
+			else if( idx > max ){
+				idx = 1;
+				rc.set("thrift:idx:" + key, idx, function(err, reply){
+					if(err) console.log(err);
+					else console.log("--> reply : " + reply );
+				});
+			}
+			rc.smembers("service:" + key, function(err, replies){
+				if(err) console.log(err);
+				else{
+					console.log("-> replies[idx] : " + idx);
+					console.dir(replies[idx-1]);
+					rc.hset("node:" + nserverName, "service:" + key, replies[idx-1], function(err, reply){
+						if(err) console.log(err);
+						else console.log("--->reply : " + reply);
+					});
+				}
+			});			
+		});
+	});
+
 	
 	if(tservers[key]) {
 		console.dir(tservers[key][i]);
